6107a60989c407c7c51749c5779a738a76f58b34
rvallbona <joan.vallbona@enti.cat>
2022-03-16 12:43:39 +0100

    Prefab Señal con Animacion.
    Cuando te chocas con una señal cambia animacion, los coches también pueden chocarse.

---
+++ Assets/Scripts/Objetos_Prefabs/ObjectReset.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectReset : MonoBehaviour
+{
+    Vector3 startPos;
+    float timer;
+    public float resetTime = 3;
+
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        startPos = this.gameObject.transform.position;
+        timer = 0;
+    }
+
+    void Update()
+    {
+
+        if (this.gameObject.transform.position.x != startPos.x || this.gameObject.transform.position.y != startPos.y)
+        {
+            timer += Time.deltaTime;
+            if (timer >= resetTime)
+            {
+                this.transform.position = startPos;
+                timer = 0;
+                rb.velocity = new Vector2(0, 0);
+                
+            }
+        }
+    }
+}
