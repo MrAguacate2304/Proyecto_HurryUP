a6747d4a9d25beb91fe42250ba804251179b8360
rvallbona <joan.vallbona@enti.cat>
2022-03-17 16:31:23 +0100

    Prefabs(SemaforoAmbar, SeñalObras, SeñalVelocidad,PhoneBooth)

---
+++ Assets/Scripts/Objetos_Prefabs/SeñalRota.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeñalRota : MonoBehaviour
+{
+    Animator animator;
+    bool rota;
+    float contador;
+    bool contadorStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        rota = false;
+        contador = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rota)
+        {
+            animator.SetBool("Rota", true);
+        }
+        if (!rota)
+        {
+            animator.SetBool("Rota", false);
+        }
+        if (contadorStart)
+        {
+            contador += Time.deltaTime;
+            if (contador >= 5)
+            {
+                rota = false;
+                contador = 0;
+                contadorStart = false;
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rota = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        contadorStart = true;
+        contador = 0;
+    }
+}
